3cc1aae4484e67b866a0bf2d43052cb1eb1bf6ff
ida02olsson <90462101+ida02olsson@users.noreply.github.com>
2021-11-20 16:55:25 +0100

    Lab 1

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -6,37 +6,49 @@ import click
 
 # Replace with your own function in Part 1
 def get_direction():
-    d_long = 0
-    d_la = 0
-    send_vel = False
-    c = click.getchar()
-    if c =='a':
-        click.echo('Left')
+ d_long = 0
+ d_la = 0
+ send_vel = False
+   # c = click.getchar()
+    #if c == '\x1b[D' or c =='a':
+ while True:
+  for event in sense.stick.get_events():
+   
+    if event.direction == "left":
+        print(event.direction, event.action)   
+ #   click.echo('Left')
         send_vel = True
         d_long = -1
         d_la = 0
-    elif c == 'd':
-        click.echo('Right')
+ #   elif c == '\x1b[C' or c == "d":
+ #       click.echo('Right')
+    elif event.direction == "right":
+        print(event.direction, event.action)
         send_vel = True
         d_long = 1
         d_la = 0
-    elif c =='w':
-        click.echo('Up')
+  #  elif c == '\x1b[A' or c =='w':
+  #      click.echo('Up')
+    elif event.direction == "up":
+        print(event.direction, event.action)
         send_vel = True
         d_long = 0
         d_la = 1
-    elif c == 's':
-        click.echo('Down')
+  #  elif c == '\x1b[B' or c == 's':
+  #      click.echo('Down')
+    elif event.direction == "down":
+        print(event.direction, event.action)
         send_vel = True
         d_long = 0
         d_la = -1
     else:
         d_long = 0
         d_la = 0
-        click.echo('Invalid input :(')
+        print(event.direction, event.action)
+       # click.echo('Invalid input :(')
         send_vel = False
     return d_long, d_la, send_vel
-
+    
 
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5001/drone"
